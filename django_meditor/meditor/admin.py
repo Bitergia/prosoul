16780c5916d3c534ea5c3405b61a8879f2311963
Alvaro del Castillo <acs@bitergia.com>
2017-12-26 16:32:36 +0100

    [data] Add data models to the admin interface of Django

--- django_meditor/meditor/admin.py
+++ django_meditor/meditor/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.Attribute)
+admin.site.register(models.DataSourceType)
+admin.site.register(models.Goal)
+admin.site.register(models.Metric)
