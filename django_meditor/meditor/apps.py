4bf0522ed665d6f200f5345470772f3058f4187c
Alvaro del Castillo <acs@bitergia.com>
2017-12-26 10:23:17 +0100

    [model] Add first version of the data model

---
+++ django_meditor/meditor/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MeditorConfig(AppConfig):
+    name = 'meditor'
---
+++ django_meditor/meditor/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class MeditorModel(models.Model):
+    """ Basic metadata for Meditor objects """
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    created_by = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
+
+
+class DataSourceType(MeditorModel):
+    name = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Metric(MeditorModel):
+    mclass = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+
+    data_source_type = models.ForeignKey(DataSourceType, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('mclass', 'name')
+
+    def __str__(self):
+        return self.data_source_type.name + " " + self.mclass + " " + self.name
+
+
+class Attribute(MeditorModel):
+    name = models.CharField(max_length=200, unique=True)
+    # Relations
+    metrics = models.ManyToManyField(Metric)
+    subattributes = models.ManyToManyField("Attribute")
+
+    def __str__(self):
+        return self.name
+
+
+class Goal(MeditorModel):
+    name = models.CharField(max_length=200, unique=True)
+    # Relations
+    attributes = models.ManyToManyField(Attribute)
+    subgoals = models.ManyToManyField("Goal")
+
+    def __str__(self):
+        return self.name
