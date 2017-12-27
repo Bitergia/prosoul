233af3b10dd8b0df6b0c4d0a50775378d9f2935c
Alvaro del Castillo <acs@bitergia.com>
2017-12-27 18:03:53 +0100

    [views] Add a simple view with the JSON data for all models
    The view is linked with the url path "/" and it just reads all
    models inside Meditor and show them in HTML using <pre></pre>
    with the JSON structure from the export tool.

--- django_meditor/meditor/views.py
+++ django_meditor/meditor/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import HttpResponse
+from meditor.meditor_export import fetch_all_models
+
+def index(request):
+    """ Basic Models Viewer just dumping the JSON of all models """
+    render_index = "<h1>Metrics Model Viewer</h1>"
+    model = fetch_all_models()
+    render_index += "<pre>"+json.dumps(model, indent=True, sort_keys=True)+"</pre>"
+    return HttpResponse(render_index)
