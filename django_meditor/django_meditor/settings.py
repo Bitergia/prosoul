cf944710a1293ef426309ae6d2ce3fd640607e92
Alvaro del Castillo <acs@bitergia.com>
2018-02-22 17:04:20 +0100

    [config] Allow meditor to be run in 172.17.0.1 and meditor
    In order to use meditor from docker containers, it is needed
    that the web server accepts 172.17.0.1 as the ip for the web server.

--- django_meditor/django_meditor/settings.py
+++ django_meditor/django_meditor/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'u1*y8wy#a!tahh!lj0fk6n)22%0_g$y-8r5q_13gf_!i8veswq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['meditor', 'localhost', '172.17.0.1']
 
 
 # Application definition
