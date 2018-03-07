c5fd6c7407ae0c9123f36937c9b3d4a82e6484ea
Alvaro del Castillo <acs@bitergia.com>
2018-03-07 06:58:31 +0100

    [tests] Add test for import/export of quality models in grimoirelab format

--- django_prosoul/prosoul/tests.py
+++ django_prosoul/prosoul/tests.py
@@ -1,3 +1,45 @@
-# from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.test import TestCase
+
+from .prosoul_import import compare_models, convert_to_grimoirelab, feed_models
+
+class ProsoulImportExport(TestCase):
+
+    def test_import_export_grimoirelab(self):
+        """ Test the import/export process for quality models in grimoirelab format """
+        format_ = 'grimoirelab'
+        filepath = 'prosoul/data/grimoirelab_ossmeter_qm.json'
+        fmodel = open(filepath, "r")
+        import_models_json = json.load(fmodel)
+        fmodel.close()
+        if format_ != "grimoirelab":
+            import_models_json = convert_to_grimoirelab(format_, import_models_json)
+        feed_models(import_models_json)
+        compare_models(import_models_json, format_)
+
+    def off_test_import_export_alambic(self):
+        """ Test the import/export process for quality models in alambic format """
+        # TODO is failing now
+        format_ = 'alambic'
+        filepath = 'prosoul/data/alambic_quality_model.json'
+        fmodel = open(filepath, "r")
+        import_models_json = json.load(fmodel)
+        fmodel.close()
+        if format_ != "grimoirelab":
+            import_models_json = convert_to_grimoirelab(format_, import_models_json)
+        feed_models(import_models_json)
+        compare_models(import_models_json, format_)
+
+    def off_test_import_export_ossmeter(self):
+        """ Test the import/export process for quality models in ossmeter format """
+        # TODO is failing now
+        format_ = 'ossmeter'
+        filepath = 'prosoul/data/ossmeter_qm.json'
+        fmodel = open(filepath, "r")
+        import_models_json = json.load(fmodel)
+        fmodel.close()
+        if format_ != "grimoirelab":
+            import_models_json = convert_to_grimoirelab(format_, import_models_json)
+        feed_models(import_models_json)
+        compare_models(import_models_json, format_)
