880ece39ff14e6074a55b38b80ff10ea3bcba05f
Alvaro del Castillo <acs@bitergia.com>
2018-04-20 05:04:24 +0200

    [pip] Add setup.py to create pip package
    Rename also django_prosoul to django-prosoul to follow the package name.

---
+++ django-prosoul/setup.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2017-2018 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1335, USA.
+#
+# Authors:
+#     Alvaro del Castillo <acs@bitergia.com>
+#
+
+import codecs
+import os
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+readme_md = os.path.join(here, 'README.md')
+
+# Pypi wants the description to be in reStrcuturedText, but
+# we have it in Markdown. So, let's convert formats.
+# Set up thinkgs so that if pypandoc is not installed, it
+# just issues a warning.
+try:
+    import pypandoc
+    README = pypandoc.convert(readme_md, 'rst')
+except (IOError, ImportError):
+    print("Warning: pypandoc module not found, or pandoc not installed. "
+          "Using md instead of rst")
+    with codecs.open(readme_md, encoding='utf-8') as f:
+        README = f.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-prosoul',
+    version='0.1.1',
+    packages=['prosoul'],
+    include_package_data=True,
+    license='GPLv3',
+    description='Prosoul is a software quality models manager to create, import/export, view and edit models',
+    long_description=README,
+    url='https://github.com/bitergia/prosoul',
+    author='Bitergia',
+    author_email='acs@bitergia.com',
+    keywords="development repositories analytics quality models assessment visualization",
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.0',
+        'Intended Audience :: Customer Service',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    install_requires=[
+        'django>=2.0',
+    ]
+)
