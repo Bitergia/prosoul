e5b4859fc0971d0f8151186ba5d55a32d1823944
Alvaro del Castillo <acs@bitergia.com>
2018-09-19 12:19:33 +0200

    [release] New version 0.1.3 with the doc formatting fixed

--- django-prosoul/setup.py
+++ django-prosoul/setup.py
@@ -46,7 +46,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-prosoul',
-    version='0.1.1',
+    version='0.1.3',
     packages=['prosoul'],
     include_package_data=True,
     license='GPLv3',
