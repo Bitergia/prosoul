79b20ccf590ca3fdea221a49a9b543d842e7ab91
Alvaro del Castillo <acs@bitergia.com>
2018-09-29 00:16:55 +0200

    Fix flake8 issues

--- django-prosoul/prosoul/views.py
+++ django-prosoul/prosoul/views.py
@@ -5,7 +5,6 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
 from django.template import loader
 from django.views import View
-from django.views.generic import RedirectView
 
 from prosoul.prosoul_export import fetch_models, gl2viewer
 from prosoul.prosoul_assess import assess
