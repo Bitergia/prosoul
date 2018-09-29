6a57f7541708fa9a99f8208a0200b20540a803fd
Alvaro del Castillo <acs@bitergia.com>
2018-09-29 11:02:20 +0200

    [rest] Add experimental API REST support for quality models
    It is based on Django REST Framework.

--- django-prosoul/django_prosoul/settings.py
+++ django-prosoul/django_prosoul/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'prosoul'
+    'prosoul',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
