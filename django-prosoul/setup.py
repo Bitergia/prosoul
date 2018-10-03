95ddc2e2e3dd7872f020cbba1ca8e767a4969d3a
Alvaro del Castillo <acs@bitergia.com>
2018-10-03 21:23:09 +0200

    [release] New version 0.4.0 in sync with the prosoul 0.4 milestone

--- django-prosoul/setup.py
+++ django-prosoul/setup.py
@@ -46,7 +46,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-prosoul',
-    version='0.2.0',
+    version='0.4.0',
     packages=['prosoul'],
     include_package_data=True,
     license='GPLv3',
