02ea064e685a2120d2ae084a2227234259ea84f7
Valerio Cosentino <valcos@bitergia.com>
2019-03-29 18:08:52 +0100

    [setup.py] Organize the list of dependencies
    This code refactors the list of dependencies, which now are
    set a a dependency per file.
    
    Signed-off-by: Valerio Cosentino <valcos@bitergia.com>

--- django-prosoul/setup.py
+++ django-prosoul/setup.py
@@ -70,8 +70,13 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
     install_requires=[
-        'django>=2.0', 'matplotlib', 'grimoire-elk', 'sortinghat', 'kidash', 'djangorestframework', 'grimoirelab'
-                                                                                                    '-toolkit '
+        'django>=2.0',
+        'matplotlib',
+        'grimoire-elk',
+        'sortinghat',
+        'kidash',
+        'djangorestframework',
+        'grimoirelab-toolkit'
     ],
     python_requires='>=3.4'
 
