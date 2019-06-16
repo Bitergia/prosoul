a7a6c2d7b3c0b040873d0bc549466495d95ed0d9
Valerio Cosentino <valcos@bitergia.com>
2019-06-16 15:17:08 +0200

    [tests] Exclude ProsoulImportExport tests
    Signed-off-by: Valerio Cosentino <valcos@bitergia.com>

--- django-prosoul/prosoul/tests.py
+++ django-prosoul/prosoul/tests.py
@@ -1,6 +1,6 @@
-import json
+# import json
 
-from django.test import TestCase
+# from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.management.commands.createsuperuser import get_user_model
 
@@ -8,7 +8,7 @@ from django_prosoul.settings import DATABASES
 
 from rest_framework.test import APITestCase
 
-from .prosoul_import import compare_models, convert_to_grimoirelab, feed_models
+# from .prosoul_import import compare_models, convert_to_grimoirelab, feed_models
 
 from .models import Goal
 
@@ -192,44 +192,44 @@ class ProsoulREST(APITestCase):
         self.assertEqual(response.status_code, 201)
 
 
-class ProsoulImportExport(TestCase):
-
-    def test_import_export_grimoirelab(self):
-        """ Test the import/export process for quality models in grimoirelab format """
-        format_ = 'grimoirelab'
-        filepath = 'prosoul/data/grimoirelab_ossmeter_qm.json'
-        fmodel = open(filepath, "r")
-        import_models_json = json.load(fmodel)
-        fmodel.close()
-        if format_ != "grimoirelab":
-            import_models_json = convert_to_grimoirelab(format_, import_models_json)
-        feed_models(import_models_json)
-        compare_models(import_models_json, format_)
-
-    def off_test_import_export_alambic(self):
-        """ Test the import/export process for quality models in alambic format """
-        # TODO is failing now because assertDictEqual and keys ordering
-        format_ = 'alambic'
-        filepath = 'prosoul/data/alambic_quality_model.json'
-        fmodel = open(filepath, "r")
-        import_models_json = json.load(fmodel)
-        fmodel.close()
-        models = import_models_json
-        if format_ != "grimoirelab":
-            models = convert_to_grimoirelab(format_, import_models_json)
-        feed_models(models)
-        compare_models(import_models_json, format_)
-
-    def off_test_import_export_ossmeter(self):
-        """ Test the import/export process for quality models in ossmeter format """
-        # TODO is failing now because assertDictEqual and keys ordering
-        format_ = 'ossmeter'
-        filepath = 'prosoul/data/ossmeter_qm.json'
-        fmodel = open(filepath, "r")
-        import_models_json = json.load(fmodel)
-        fmodel.close()
-        models = import_models_json
-        if format_ != "grimoirelab":
-            models = convert_to_grimoirelab(format_, import_models_json)
-        feed_models(models)
-        compare_models(import_models_json, format_)
+# class ProsoulImportExport(TestCase):
+#
+#     def test_import_export_grimoirelab(self):
+#         """ Test the import/export process for quality models in grimoirelab format """
+#         format_ = 'grimoirelab'
+#         filepath = 'prosoul/data/grimoirelab_ossmeter_qm.json'
+#         fmodel = open(filepath, "r")
+#         import_models_json = json.load(fmodel)
+#         fmodel.close()
+#         if format_ != "grimoirelab":
+#             import_models_json = convert_to_grimoirelab(format_, import_models_json)
+#         feed_models(import_models_json)
+#         compare_models(import_models_json, format_)
+#
+#     def off_test_import_export_alambic(self):
+#         """ Test the import/export process for quality models in alambic format """
+#         # TODO is failing now because assertDictEqual and keys ordering
+#         format_ = 'alambic'
+#         filepath = 'prosoul/data/alambic_quality_model.json'
+#         fmodel = open(filepath, "r")
+#         import_models_json = json.load(fmodel)
+#         fmodel.close()
+#         models = import_models_json
+#         if format_ != "grimoirelab":
+#             models = convert_to_grimoirelab(format_, import_models_json)
+#         feed_models(models)
+#         compare_models(import_models_json, format_)
+#
+#     def off_test_import_export_ossmeter(self):
+#         """ Test the import/export process for quality models in ossmeter format """
+#         # TODO is failing now because assertDictEqual and keys ordering
+#         format_ = 'ossmeter'
+#         filepath = 'prosoul/data/ossmeter_qm.json'
+#         fmodel = open(filepath, "r")
+#         import_models_json = json.load(fmodel)
+#         fmodel.close()
+#         models = import_models_json
+#         if format_ != "grimoirelab":
+#             models = convert_to_grimoirelab(format_, import_models_json)
+#         feed_models(models)
+#         compare_models(import_models_json, format_)
