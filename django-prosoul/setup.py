12c51097465d58576bf0204e57836f1512e1184a
Valerio Cosentino <valcos@bitergia.com>
2019-07-16 09:41:28 +0200

    [setup] Fix format type for long description of package
    Pypi requires that the format of the long description for a
    package is detailed in the variable `long_description_content_type`,
    in setup.py. Since it now supports Markdown, the conversion to
    reST is not needed anymore.
    
    Signed-off-by: Valerio Cosentino <valcos@bitergia.com>

--- django-prosoul/setup.py
+++ django-prosoul/setup.py
@@ -28,18 +28,9 @@ from setuptools import setup
 here = os.path.abspath(os.path.dirname(__file__))
 readme_md = os.path.join(here, 'README.md')
 
-# Pypi wants the description to be in reStrcuturedText, but
-# we have it in Markdown. So, let's convert formats.
-# Set up thinkgs so that if pypandoc is not installed, it
-# just issues a warning.
-try:
-    import pypandoc
-    README = pypandoc.convert(readme_md, 'rst')
-except (IOError, ImportError):
-    print("Warning: pypandoc module not found, or pandoc not installed. "
-          "Using md instead of rst")
-    with codecs.open(readme_md, encoding='utf-8') as f:
-        README = f.read()
+# Get the package description from the README.md file
+with codecs.open(readme_md, encoding='utf-8') as f:
+    long_description = f.read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
@@ -51,7 +42,8 @@ setup(
     include_package_data=True,
     license='GPLv3',
     description='Prosoul is a software quality models manager to create, import/export, view and edit models',
-    long_description=README,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/bitergia/prosoul',
     author='Bitergia',
     author_email='acs@bitergia.com',
